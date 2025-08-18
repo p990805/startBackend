7564db5a6acfcf6c70eaa753c4ed4f1278b117cf
박주찬 <p990805@naver.com>
2025-08-18 23:27:24 +0900

    Feat : User entity(JPA 작성)

---
+++ demo/src/main/java/com/example/entity/User.java
@@ -0,0 +1,44 @@
+package com.example.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+@Entity
+@Table(name = "users",
+        uniqueConstraints = {
+            @UniqueConstraint(columnNames = "username"),
+            @UniqueConstraint(columnNames = "email"),
+            @UniqueConstraint(columnNames = "nickname")
+        }
+)
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 20, unique = true)
+    private String username;
+
+    @Column(nullable = false, length = 100)
+    private String password;
+
+    @Column(nullable = false, length = 20, unique = true)
+    private String nickname;
+
+    @Column(nullable = false, length = 50, unique = true)
+    private String email;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+}
